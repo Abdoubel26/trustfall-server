a823ad5a02aaa5e7d5893cefe12f58a6958ac780
Abdoubel26 <abdoubelounis09@gmail.com>
2026-07-23 16:32:27 +0100

    add dynamic frontend url

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import crypto from "crypto";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 const server = http.createServer(app);
 
+
+
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: process.env.FRONTEND_URL || "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 });
@@ -106,6 +111,9 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5000, () => {
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
     console.log("🚀 Realtime engine online on port 5000");
 });
